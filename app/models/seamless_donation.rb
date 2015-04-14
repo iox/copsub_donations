90b81da3e5bb717dacdc7ab1a27015e026df0f74
Ignacio Huerta <ignacio@ihuerta.net>
2015-04-14 10:16:21 +0200

    Fix issue with duplicated donations

--- app/models/seamless_donation.rb
+++ app/models/seamless_donation.rb
@@ -7,6 +7,7 @@ class SeamlessDonation < ActiveRecord::Base
                 joins("LEFT JOIN #{PREFIX}postmeta currency ON #{PREFIX}posts.id = currency.post_id AND currency.meta_key = '_dgx_donate_donation_currency'").
                 joins("LEFT JOIN #{PREFIX}postmeta amount ON #{PREFIX}posts.id = amount.post_id AND amount.meta_key = '_dgx_donate_amount'").
                 joins("LEFT JOIN #{PREFIX}postmeta donation_method ON #{PREFIX}posts.id = donation_method.post_id AND donation_method.meta_key = '_dgx_donate_payment_processor'").
-                joins("LEFT JOIN #{PREFIX}postmeta email ON #{PREFIX}posts.id = email.post_id AND email.meta_key = '_dgx_donate_donor_email'")
+                joins("LEFT JOIN #{PREFIX}postmeta email ON #{PREFIX}posts.id = email.post_id AND email.meta_key = '_dgx_donate_donor_email'").
+                group("#{PREFIX}posts.id")
 
 end
