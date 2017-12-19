b31b498fb019da0fce9c1376c0c7fb92fb0bca9d
Ignacio Huerta <ignacio@ihuerta.net>
2017-12-19 08:01:52 +0100

    Add graphs, fix ordering in donations report

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -28,16 +28,16 @@ class FrontController < ApplicationController
       @alert = "Warning: The last registered bank donation occurred on #{I18n.l(last_bank_donation_date)}. This means that no bank statements have been imported into the donations app since then. This report could show incorrect lost regular donors. Please import the latest bank statement to update this report."
     end
     
-    @new_regular_donors = Donor.where("((role = 'recurring_supporter' OR selected_donor_type = 'supporter') AND first_donated_at >= ? AND first_donated_at <= ?) OR (role = 'inactive_supporter' AND last_donated_at >= ? AND last_donated_at <= ?)", @start_date, @end_date, @start_date, @end_date).order("first_donated_at")
+    @new_regular_donors = Donor.where("((role = 'recurring_supporter' OR selected_donor_type = 'supporter') AND first_donated_at >= ? AND first_donated_at <= ?) OR (role = 'inactive_supporter' AND last_donated_at >= ? AND last_donated_at <= ?)", @start_date, @end_date, @start_date, @end_date).order("last_donated_at desc")
 
     @lost_regular_donors = Donor.where("stopped_regular_donations_date IS NOT NULL AND stopped_regular_donations_date >= ? AND stopped_regular_donations_date <= ?", @start_date, @end_date).order("stopped_regular_donations_date desc")
     
     @current_monthly_income_regular_donors = Donor.where("role = 'recurring_supporter'")
     
-    @regular_donations_monthly = Donation.where(other_income: false).where("category_id != 5").where("donated_at > ?", @start_date - 1.year).order("donated_at").group("date_format(donated_at, '%M %Y')").sum(:amount_in_dkk)
-    @onetime_donations_monthly = Donation.where(other_income: false).where("category_id  = 5").where("donated_at > ?", @start_date - 1.year).order("donated_at").group("date_format(donated_at, '%M %Y')").sum(:amount_in_dkk)
+    @regular_donations_monthly = Donation.where(other_income: false).where("category_id != 5").where("donated_at >= ?", (@start_date - 1.year).beginning_of_month).order("donated_at").group("date_format(donated_at, '%M %Y')").sum(:amount_in_dkk)
+    @onetime_donations_monthly = Donation.where(other_income: false).where("category_id  = 5").where("donated_at >= ?", (@start_date - 1.year).beginning_of_month).order("donated_at").group("date_format(donated_at, '%M %Y')").sum(:amount_in_dkk)
     
-    @onetime_donations = Donation.where(other_income: false).where(category_id: 5).where("donated_at >= ? AND donated_at <= ?", @start_date, @end_date)
+    @onetime_donations = Donation.where(other_income: false).where(category_id: 5).where("donated_at >= ? AND donated_at <= ?", @start_date, @end_date).order("donated_at desc")
   end
 
 end
